e293a840435133478536d12d1cd3771d551774e6
sasaki <skyo.97.admj@gmail.com>
2021-05-11 15:17:28 +0900

    【pages】add_post

---
+++ frontend/pages/post/index.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent, ref, computed, useContext } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  middleware: 'login_redirect',
+  setup() {},
+});
+</script>
+
+<style>
+</style>
