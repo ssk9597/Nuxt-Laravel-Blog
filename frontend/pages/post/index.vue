9a2e813ec55a28a5fef79bf7b66eaacc5e0e1119
sasaki <skyo.97.admj@gmail.com>
2021-05-12 14:08:54 +0900

    Add page_post

--- frontend/pages/post/index.vue
+++ frontend/pages/post/index.vue
@@ -1,13 +1,105 @@
-<template></template>
+<template>
+  <div>
+    <client-only>
+      <div class="post-container">
+        <input class="post-form" :placeholder="titlePlaceholder" type="text" v-model="title" />
+        <input class="post-form" :placeholder="urlPlaceholder" type="text" v-model="url" />
+
+        <vue-editor class="post-content" v-model="content" />
+        <button class="post-btn" @click="sendContent">投稿を保存する</button>
+      </div>
+    </client-only>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent, ref, computed, useContext } from '@nuxtjs/composition-api';
+import { VueEditor } from 'vue2-editor';
 
 export default defineComponent({
-  middleware: 'login_redirect',
-  setup() {},
+  // middleware: 'login_redirect',
+  components: { VueEditor },
+  setup() {
+    // data
+    const title = ref<string>('');
+    const url = ref<string>('');
+    const titlePlaceholder = ref<string>('ここにタイトルを入力してください');
+    const urlPlaceholder = ref<string>('ここにURLを入力してください');
+    const content = ref<string>('');
+
+    //methods
+    const sendContent = async () => {
+      try {
+        console.log(content.value);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    return {
+      // data
+      title,
+      url,
+      titlePlaceholder,
+      urlPlaceholder,
+      content,
+      // methods
+      sendContent,
+    };
+  },
 });
 </script>
 
-<style>
+<style lang="scss" scoped>
+.post-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 30px;
+}
+.post-form {
+  box-sizing: border-box;
+  display: block;
+  max-width: 600px;
+  width: 100%;
+  height: 30px;
+  padding: 0 10px;
+  margin: 10px 0;
+  font-size: 12px;
+  color: #333;
+  border: solid 1px #ccc;
+  border-radius: 6px;
+  outline: 0;
+  transition: 0.3s;
+  &:focus {
+    border: 1px solid #00b5ad;
+  }
+}
+
+.post-content {
+  margin-top: 20px;
+  max-width: 600px;
+  width: 100%;
+}
+
+.post-btn {
+  max-width: 300px;
+  width: 100%;
+  display: inline-block;
+  margin-top: 30px;
+  font-size: 1.25rem;
+  padding: 0.5rem 0;
+  color: #00b5ad;
+  background: #fff;
+  text-decoration: none;
+  user-select: none;
+  border: 1px #00b5ad solid;
+  border-radius: 20px;
+  transition: 0.4s ease;
+  cursor: pointer;
+  &:hover {
+    color: #fff;
+    background: #00b5ad;
+  }
+}
 </style>
