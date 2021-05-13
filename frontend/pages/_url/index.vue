11bae599c616f6257f23c9c25c1cc579cda327f8
sasaki <skyo.97.admj@gmail.com>
2021-05-13 19:49:34 +0900

    Add Axios_url_get

--- frontend/pages/_url/index.vue
+++ frontend/pages/_url/index.vue
@@ -1,7 +1,56 @@
-<template></template>
+<template>
+  <div v-if="isEdit">
+    <h2>詳細ページ</h2>
+    <h3>{{ post.title }}</h3>
+    <p v-html="post.content"></p>
+    <button @click="editContent">編集する</button>
+  </div>
+</template>
 
-<script>
-export default {};
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  computed,
+  useContext,
+  useRoute,
+  onMounted,
+} from '@nuxtjs/composition-api';
+
+// cookie
+import $cookie from 'cookie-universal-nuxt';
+
+export default defineComponent({
+  setup(props, context) {
+    // axios
+    const { $axios, store } = useContext();
+    // route
+    const route = useRoute();
+
+    // data
+    const post = ref<string>('');
+    const isEdit = ref<Boolean>(true);
+
+    // mounted
+    onMounted(async () => {
+      const url = computed((): String => route.value.params.url);
+      const res = await $axios.get(`/posts/show/${url.value}`);
+      post.value = res.data;
+      console.log(post.value);
+    });
+
+    // methods
+    const editContent = () => {
+      isEdit.value = !isEdit.value;
+    };
+
+    return {
+      // data
+      post,
+      isEdit,
+      // methods
+      editContent,
+    };
+  },
+});
 </script>
-
-<style></style>
